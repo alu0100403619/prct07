ed4389b504d01ebadf000bdf48b0b79b3fb746ad
Gonzalo Garcia <alu0100403619@ull.edu.es>
2013-10-23 17:21:08 +0100

    Error no definidos metodos num y den

--- spec/fraccion_spec.rb
+++ spec/fraccion_spec.rb
@@ -2,7 +2,15 @@ require "lib/fraccion.rb"
 
 describe Fraccion do
   before :each do
-    @f = Fraccion.new(1,2)
+    @f = Fraccion.new(2,4)
+  end  
+end
+
+describe "# almacenamiento numerador y denominador" do
+  it "Debe existir un numerador" do
+    @f.num.should eq(2)
   end
-  
+  it "Debe existir un denominador" do
+    @f.den.should eq(4)
+  end
 end
